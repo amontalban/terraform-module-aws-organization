7621b323024756f0b8cd6329244f5eadc4cb5bd2
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-02-20 11:39:55 -0500

    fix: adds concat in outputs to handle no organization created

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,26 @@
 output "arn" {
   description = "ARN of the organization."
-  value       = "${element(aws_organizations_organization.organization.*.arn, 0)}"
+  value       = "${element(concat(aws_organizations_organization.organization.*.arn, list("")), 0)}"
 }
 
 output "id" {
   description = "Id of the organization."
-  value       = "${element(aws_organizations_organization.organization.*.id, 0)}"
+  value       = "${element(concat(aws_organizations_organization.organization.*.id, list(""), 0)}"
 }
 
 output "master_account_arn" {
   description = "Main (root) account ARN."
-  value       = "${element(aws_organizations_organization.organization.*.master_account_arn, 0)}"
+  value       = "${element(concat(aws_organizations_organization.organization.*.master_account_arn, list(""), 0)}"
 }
 
 output "master_account_email" {
   description = "Main (root) account email."
-  value       = "${element(aws_organizations_organization.organization.*.master_account_email, 0)}"
+  value       = "${element(concat(aws_organizations_organization.organization.*.master_account_email, list(""), 0)}"
 }
 
 output "master_account_id" {
   description = "Main (root) account id."
-  value       = "${element(aws_organizations_organization.organization.*.master_account_id, 0)}"
+  value       = "${element(concat(aws_organizations_organization.organization.*.master_account_id, list(""), 0)}"
 }
 
 output "account_names" {
