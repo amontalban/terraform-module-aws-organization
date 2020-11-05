cfd1b4c0c7b0e2da82b74599fc74579329f0ab76
Julien Cabillot <julien.cabillot@fxinnovation.com>
2020-11-05 14:13:44 -0500

    feat (BREAKING): terraform 0.12 upgrade

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+# 1.0.0
+
+* feat (BREAKING): terraform 0.12 upgrade
+
 # 0.2.1
 
 * chore: bump pre-commit
--- examples/standard-organization/outputs.tf
+++ examples/standard-organization/outputs.tf
@@ -1,27 +1,27 @@
 output "arn" {
-  value = "${module.organization.arn}"
+  value = module.organization.arn
 }
 
 output "id" {
-  value = "${module.organization.id}"
+  value = module.organization.id
 }
 
 output "master_account_arn" {
-  value = "${module.organization.master_account_arn}"
+  value = module.organization.master_account_arn
 }
 
 output "master_account_email" {
-  value = "${module.organization.master_account_email}"
+  value = module.organization.master_account_email
 }
 
 output "master_account_id" {
-  value = "${module.organization.master_account_id}"
+  value = module.organization.master_account_id
 }
 
 output "account_names" {
-  value = "${module.organization.account_names}"
+  value = module.organization.account_names
 }
 
 output "account_ids" {
-  value = "${module.organization.account_ids}"
+  value = module.organization.account_ids
 }
